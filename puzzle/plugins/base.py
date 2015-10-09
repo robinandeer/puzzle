551a0a232951f383ce1bb88c40405ee61ef5417e
moonso <monsunas@gmail.com>
2015-10-09 10:05:15 +0200

    Added models

---
+++ puzzle/plugins/base.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from path import path
+from vcf_parser import VCFParser
+
+
+class Plugin(object):
+    """docstring for Plugin"""
+
+    def init_app(self, app):
+        """Initialize plugin via Flask."""
+        self.root_path = app.config['PUZZLE_ROOT']
+        self.pattern = app.config['PUZZLE_PATTERN']
+
+    def cases(self, pattern=None):
+        """Return all cases."""
+        raise NotImplementedError
+
+    def variants(self, case_id, skip=0, count=30, gene_list=None):
+        """Return count variants for a case.
+        
+        """
+        raise NotImplementedError
+        
+    def variant(self, variant_id):
+        """Return a specific variant."""
+        raise NotImplementedError
--- puzzle/plugins/vcf.py
+++ puzzle/plugins/vcf.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from path import path
-from vcf_parser import VCFParser
+
+from puzzle.models import Variant
+
+from vcftoolbox import get_variant_dict, HeaderParser
 
 
 class Plugin(object):
@@ -13,35 +16,63 @@ class Plugin(object):
         self.root_path = app.config['PUZZLE_ROOT']
         self.pattern = app.config['PUZZLE_PATTERN']
 
-    def _find_vcfs(self, pattern=None):
+    def _find_vcfs(self, pattern = '*.vcf'):
         """Walk subdirectories and return VCF files."""
-        pattern = pattern or self.pattern
         return path(self.root_path).walkfiles(pattern)
 
-    def vcf_files(self, pattern=None):
+    def cases(self, pattern=None):
         """Return all VCF file paths."""
         pattern = pattern or self.pattern
         return self._find_vcfs(pattern)
 
-    def load_vcf(self, vcf_path):
-        """Load a new VCF file."""
-        self.vcf_file = vcf_path
+    def _variants(self, vcf_file_path):
+        head = HeaderParser()
+        #Parse the header
+        with open(vcf_file_path, 'r') as variant_file:
+            for line in variant_file:
+                line = line.rstrip()
+                if line.startswith('#'):
+                    if line.startswith('##'):
+                        head.parse_meta_data(line)
+                    else:
+                        head.parse_header_line(line)
+                else:
+                    break
+        
+        header_line = head.header
+        if len(header_line) > 8:
+            variant_columns = header_line[:9]
+        else:
+            variant_columns = header_line[:8]
+        
+        with open(vcf_file_path, 'r') as vcf_file:
+            index = 0
+            for variant_line in vcf_file:
+                if not variant_line.startswith('#'):
+                    index += 1
+                    variant_dict =  get_variant_dict(
+                        variant_line = variant_line,
+                        header_line = header_line
+                    )
+                    variant = Variant(
+                        **{column: variant_dict[column] for column in variant_columns}
+                        )
+                    print(variant)
+        #
+        # for index, variant in enumerate(variants):
+        #     variant['id'] = index
+        #     variant['index'] = index + 1
+        #     variant['start'] = int(variant['POS'])
+        #     variant['stop'] = int(variant['POS']) + (len(variant['REF'])
+        #                                              - len(variant['ALT']))
+        #     yield variant
 
-    def _variants(self):
-        variants = VCFParser(self.vcf_file, check_info=False)
-        for index, variant in enumerate(variants):
-            variant['id'] = index
-            variant['index'] = index + 1
-            variant['start'] = int(variant['POS'])
-            variant['stop'] = int(variant['POS']) + (len(variant['REF'])
-                                                     - len(variant['ALT']))
-            yield variant
-
-    def variants(self, skip=0, count=30, gene_list=None):
+    def variants(self, case_id, skip=0, count=30, gene_list=None):
         """Return all variants in the VCF."""
+        
         limit = count + skip
         if gene_list:
-            filtered_variants = (variant for variant in self._variants()
+            filtered_variants = (variant for variant in self._variants(case_id)
                                  if gene_list in
                                  variant['info_dict']['Clinical_db_gene_annotation'])
         else:
@@ -60,3 +91,10 @@ class Plugin(object):
             if variant['variant_id'] == variant_id:
                 return variant
         return None
+
+
+if __name__ == '__main__':
+    vcf_file = "tests/fixtures/15031.vcf"
+    plugin = Plugin()
+    plugin._variants(vcf_file)
+    print("hej")
