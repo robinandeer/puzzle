88b2937466e1e26a8b8004779a612ecde9e548a5
moonso <monsunas@gmail.com>
2015-10-13 22:39:34 +0200

    Added test for omim id:s with phenotype

--- tests/utils/test_get_omim_number.py
+++ tests/utils/test_get_omim_number.py
@@ -8,3 +8,8 @@ def test_get_omim_number():
 
 def test_get_non_existing_omim():
     assert get_omim_number('HEJ') == None
+
+def test_get_phenotype():
+    assert get_omim_number('MCCRP2') != get_omim_number('PLK4')
+    assert get_omim_number('MCCRP2') == None
+    assert get_omim_number('PLK4') == 605031
