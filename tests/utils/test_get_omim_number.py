d13bfa5b1a3b0e079b510bae6c06d19dc2ccf5ed
moonso <monsunas@gmail.com>
2015-10-13 21:01:29 +0200

    Updated structure for tests and added test for get_hgnc_symbol

--- tests/utils/test_get_omim_number.py
+++ tests/utils/test_get_omim_number.py
@@ -5,3 +5,6 @@ from puzzle.utils import (get_most_severe_consequence, get_hgnc_symbols,
 
 def test_get_omim_number():
     assert get_omim_number('IFT172') == 607386
+
+def test_get_non_existing_omim():
+    assert get_omim_number('HEJ') == None
