5fcd9d40aba51e02962c5882a3db0d2435dae25d
moonso <monsunas@gmail.com>
2015-10-14 11:21:45 +0200

    Added tests for variant and updated docstrings

--- puzzle/models/variant.py
+++ puzzle/models/variant.py
@@ -65,36 +65,92 @@ class Variant(dict):
             Args:
                 name (str): The name of the frequency field
         """
+        logger.debug("Adding frequency {0} with value {1} to variant {2}".format(
+            name, value, self['variant_id']))
         self['frequencies'].append({name: value})
 
-    def add_severities(self, name, value):
-        """docstring for add_frequency"""
-        self['frequencies'].append({name: value})
+    def add_severity(self, name, value):
+        """Add a severity to the variant
+        
+            Args:
+                name (str): The name of the severity
+                value : The value of the severity
+        """
+        logger.debug("Adding severity {0} with value {1} to variant {2}".format(
+            name, value, self['variant_id']))
+        self['severities'].append({name: value})
 
     def add_individual(self, genotype):
-        """Add the information for a individual"""
+        """Add the information for a individual
+            
+            This adds a genotype dict to variant['individuals']
+            
+            Args:
+                genotype (dict): A genotype dictionary
+        """
+        logger.debug("Adding genotype {0} to variant {1}".format(
+            genotype, self['variant_id']))
         self['individuals'].append(genotype)
 
     def add_transcript(self, transcript):
-        """Add the information transcript"""
+        """Add the information transcript
+        
+            This adds a transcript dict to variant['transcripts']
+            
+            Args:
+                transcript (dict): A transcript dictionary
+        """
+        logger.debug("Adding transcript {0} to variant {1}".format(
+            transcript, self['variant_id']))
         self['transcripts'].append(transcript)
 
     def add_gene(self, gene):
-        """Add the information of a gene"""
+        """Add the information of a gene
+        
+            This adds a gene dict to variant['genes']
+            
+            Args:
+                gene (dict): A gene dictionary
+        
+        """
+        logger.debug("Adding gene {0} to variant {1}".format(
+            gene, self['variant_id']))
         self['genes'].append(gene)
 
     def add_compound(self, compound):
-        """Add the information of a compound variant"""
+        """Add the information of a compound variant
+        
+            This adds a compound dict to variant['compounds']
+        
+            Args:
+                compound (dict): A compound dictionary
+        
+        """
+        logger.debug("Adding compound {0} to variant {1}".format(
+            compound, self['variant_id']))
         self['compounds'].append(compound)
-
+    
+    def update_variant_id(self, variant_id):
+        """Update the variant id for an individual
+        
+            Args:
+                variant_id (str): A variant id
+        """
+        
+        self._set_variant_id(variant_id=variant_id)
+        
     def _set_variant_id(self, variant_id=None):
         """Set the variant id for this variant"""
-        if variant_id:
-            self['variant_id'] = variant_id
-        else:
-            self['variant_id'] = '_'.join([
+        if not variant_id:
+            variant_id = '_'.join([
                 self['CHROM'],
                 self['POS'],
                 self['REF'],
                 self['ALT']
                 ])
+        
+        logger.debug("Updating variant id to {0}".format(
+            variant_id))
+        
+        self['variant_id'] = variant_id
+        
