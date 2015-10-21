31f2303a0ab80abe804de0c03d550aefc2ab8916
Robin Andeer <robin.andeer@gmail.com>
2015-10-21 19:08:21 +0200

    remove vcf

--- tests/plugins/test_vcf.py
+++ tests/plugins/test_vcf.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
 from puzzle.plugins import VcfPlugin
 
-##TODO define a vcf in the test
-vcf = "tests/fixtures/15031.vcf"
+# TODO: define a vcf in the test
+vcf = "tests/fixtures/hapmap.vcf"
+
 
 def test_variants():
     vcf_plugin = VcfPlugin()
@@ -14,10 +14,10 @@ def test_variants():
 
 def test_variant():
     vcf_plugin = VcfPlugin()
-    variant = vcf_plugin.variant(vcf, 'X_132888207_TA_T')
+    variant = vcf_plugin.variant(vcf, 'X_155239821_G_A')
     assert variant['CHROM'] == 'X'
-    assert variant['POS'] == '132888207'
+    assert variant['POS'] == '155239821'
 
     # get 10th variant
-    variant = vcf_plugin.variant(vcf, '9_139366460_C_T')
+    variant = vcf_plugin.variant(vcf, '3_124998098_C_A')
     assert variant['index'] == 10
--- tests/test_blueprints_variants.py
+++ tests/test_blueprints_variants.py
@@ -4,10 +4,10 @@ from flask import url_for
 
 def test_variants(client):
     res = client.get(url_for('variants.variants',
-                     case_id='tests|fixtures|15031.vcf'))
+                     case_id='tests|fixtures|hapmap.vcf'))
     assert res.status_code == 200
 
     # test with skip
     res = client.get(url_for('variants.variants',
-                     case_id='tests|fixtures|15031.vcf', skip=30))
+                     case_id='tests|fixtures|hapmap.vcf', skip=30))
     assert res.status_code == 200
