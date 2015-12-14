fcab0b9e8892bccd626966d311097462568ee755
Vasilios Zachariadis <vasilios.zachariadis@ki.se>
2015-12-14 13:17:01 +0100

    fix cytoband tests

--- tests/utils/test_get_cytoband.py
+++ tests/utils/test_get_cytoband.py
@@ -4,19 +4,19 @@ def test_get_cytoband_coord():
     chrom = '1'
     pos = 3
     
-    assert get_cytoband_coord(chrom, pos) == '1.p36.33'
+    assert get_cytoband_coord(chrom, pos) == '1p36.33'
 
 def test_get_cytoband_coord_chr():
     chrom = 'chr1'
     pos = 3
     
-    assert get_cytoband_coord(chrom, pos) == '1.p36.33'
+    assert get_cytoband_coord(chrom, pos) == '1p36.33'
 
 def test_get_cytoband_coord_str_pos():
     chrom = 'chr1'
     pos = '3'
     
-    assert get_cytoband_coord(chrom, pos) == '1.p36.33'
+    assert get_cytoband_coord(chrom, pos) == '1p36.33'
 
 def test_get_cytoband_coord_non_existing_chr():
     chrom = 'chrMT'
