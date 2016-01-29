b950344c456398ee800eb51382811267e13c3337
moonso <monsunas@gmail.com>
2016-01-29 11:12:11 +0100

    Updated test for test case

--- tests/models/test_models_case.py
+++ tests/models/test_models_case.py
@@ -5,9 +5,9 @@ from puzzle.models.case import Case
 def test_Case():
     # with explicit name
     case_obj = Case('sample1', name='Oscar Wilde')
-    assert case_obj['id'] == 'sample1'
-    assert case_obj['name'] == 'Oscar Wilde'
+    assert case_obj.case_id == 'sample1'
+    assert case_obj.name == 'Oscar Wilde'
 
     # without name
     case_obj = Case('sample2')
-    assert case_obj['id'] == case_obj['name'] == 'sample2'
+    assert case_obj.case_id == case_obj.name == 'sample2'
