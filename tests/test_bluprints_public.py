4ae709981aa4ea627862e98dfbf89406c701bbb3
moonso <monsunas@gmail.com>
2016-01-29 11:18:45 +0100

    Removed tests for blueprints

--- tests/test_bluprints_public.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import url_for
-
-
-def test_index(client):
-    res = client.get(url_for('public.index'))
-    assert res.status_code == 200
-    assert b'hapmap.vcf' in res.data
