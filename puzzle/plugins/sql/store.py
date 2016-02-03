0102bcfc97f19218acf1ca9305527b868d5d4185
Robin Andeer <robin.andeer@gmail.com>
2016-02-03 10:32:03 +0100

    convert gene_list filter in sql-adapter to gene_ids filter in adapters

--- puzzle/plugins/sql/store.py
+++ puzzle/plugins/sql/store.py
@@ -3,6 +3,7 @@
 puzzle.plugins.sql.store
 ~~~~~~~~~~~~~~~~~~
 """
+import itertools
 import logging
 import os
 
@@ -205,10 +206,21 @@ class Store(Plugin):
 
     def variants(self, case_id, skip=0, count=30, filters=None):
         """Fetch variants for a case."""
+        filters = filters or {}
         logger.debug("Fetching case with case_id:{0}".format(case_id))
         case_obj = self.case(case_id)
         plugin, case_id = select_plugin(case_obj)
         self.filters = plugin.filters
+
+        gene_lists = (self.gene_list(list_id) for list_id
+                      in filters.get('gene_lists', []))
+        nested_geneids = (gene_list.gene_ids for gene_list in gene_lists)
+        gene_ids = set(itertools.chain.from_iterable(nested_geneids))
+
+        if filters.get('gene_ids'):
+            filters['gene_ids'].extend(gene_ids)
+        else:
+            filters['gene_ids'] = gene_ids
         variants = plugin.variants(case_id, skip, count, filters)
         return variants
 
