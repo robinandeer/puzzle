44e3e306248b9f468dcd78fc5eaf75502697c5d7
Robin Andeer <robin.andeer@gmail.com>
2016-02-12 15:08:23 +0100

    Test get_gen_info method :jack_o_lantern:

--- puzzle/utils/__init__.py
+++ puzzle/utils/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .get_info import (get_most_severe_consequence, get_omim_number,
-                      get_cytoband_coord, get_gene_info)
+                       get_cytoband_coord, get_gene_info)
 from .ped import get_individuals, get_case
 from .phenomizer import hpo_genes
 from .constants import IMPACT_SEVERITIES
