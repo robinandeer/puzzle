97377160e50f1ad815f42e33a8327da8dc630eff
moonso <monsunas@gmail.com>
2016-02-18 21:11:05 +0100

    Reverted changes

---
+++ puzzle/plugins/base_case_mixin.py
@@ -0,0 +1,16 @@
+
+class BaseCaseMixin(object):
+    "Base plugin for caase mixins"
+    
+    def cases(self, pattern=None):
+        """Return all cases."""
+        raise NotImplementedError
+
+    def case(self, case_id=None):
+        """Return a case."""
+        raise NotImplementedError
+
+    def individual(self, ind_id=None):
+        """Return a individual."""
+        raise NotImplementedError
+    
