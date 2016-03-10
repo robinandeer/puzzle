fbdbf39ed3c22afb8cec2bf2a6f9e3ae6b0a4dd8
moonso <monsunas@gmail.com>
2016-03-10 15:51:48 +0100

    Refactored vcf adapter since case search is done outside now

--- puzzle/plugins/vcf/vcf.py
+++ puzzle/plugins/vcf/vcf.py
@@ -15,62 +15,28 @@ logger = logging.getLogger(__name__)
 class VcfPlugin(VariantMixin, CaseMixin, Plugin):
     """docstring for Plugin"""
 
-    def __init__(self, root_path=None, case_lines=None,
-                 case_type=None, pattern='*.vcf', vtype='snv',
-                 case_obj=None):
+    def __init__(self, variant_type='snv'):
         """Initialize a vcf adapter.
 
             When instansiating all cases are found.
 
             Args:
-                root_path(str) : Path to a vcf file or a dir with vcf files
-                case_lines(Iterable) : Lines with ped like information
-                case_type(str) : Format of pedigreeinformation
-                patter(str) : What pattern to search for in directory
-                vtype(str) : 'snv' or 'sv'
-                case_obj(puzzle.models.case) : If initialized with a case
+                variant_type(str) : 'snv' or 'sv'
         """
         super(VcfPlugin, self).__init__()
 
         self.individuals = []
         self.case_objs = []
-        logger.info("Updating root path to {0}".format(root_path))
-        self.root_path = root_path
 
-        self.check_setup(case_lines)
+        self.variant_type = variant_type
+        logger.info("Setting variant type to {0}".format(variant_type))
 
-        self.variant_type = vtype
-        logger.info("Setting variant type to {0}".format(vtype))
-
-        self.pattern = pattern
-        logger.debug("Updating pattern to {0}".format(pattern))
-        
         self.variant_columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER']
-        
         
         self.head = None
         self.vep_header = None
         self.snpeff_header = None
         
-        
-        if root_path:
-            if os.path.isdir(root_path):
-                logger.info("Looking for vcf files in {0}".format(root_path))
-                for vcf_file in self._find_vcfs(pattern=pattern):
-                    logger.info("Found vcf {0}".format(vcf_file))
-                    case_obj = get_case(variant_source=vcf_file)
-                    self.case_objs.append(case_obj)
-            else:
-                self.case_objs.append(get_case(
-                    variant_source=self.root_path,
-                    case_lines=case_lines,
-                    case_type=case_type
-                    )
-                )
-
-            for case_obj in self.case_objs:
-                for ind in case_obj.individuals:
-                    self.individuals.append(ind)
         
         self.filters.can_filter_gene = True
         self.filters.can_filter_frequency = True
@@ -80,32 +46,6 @@ class VcfPlugin(VariantMixin, CaseMixin, Plugin):
         self.filters.can_filter_sv = True
         self.filters.can_filter_sv_len = True
         self.filters.can_filter_inheritance = True
-
-    def check_setup(self, case_lines):
-        """Make some small tests to see if setup is correct"""
-        valid_vcf_suffixes = ('.vcf', '.vcf.gz')
-        if self.root_path:
-            if case_lines:
-                # If family file we only allow one vcf file as input
-                if not os.path.isfile(self.root_path):
-                    raise SyntaxError("Variant source has to be a vcf file when"\
-                                     " running with family file")
-            if os.path.isfile(self.root_path):
-                if not self.root_path.endswith(valid_vcf_suffixes):
-                    raise SyntaxError("Vcf file has to end with with .vcf or .vcf.gz")
-        return
-
-
-    def _find_vcfs(self, pattern='*.vcf'):
-        """Walk subdirectories and return VCF files.
-
-            Args:
-                pattern (str): What pattern to serch for in filenames
-
-            Return:
-                paths (Iterable): The paths found
-        """
-        return path(self.root_path).walkfiles(pattern)
 
     def init_app(self, app):
         """Initialize plugin via Flask."""
